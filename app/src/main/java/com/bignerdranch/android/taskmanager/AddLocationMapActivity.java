90d1a5e6cff7f9880afaa73b051f8d8914c9e23d
padillae <padillae@appstate.edu>
2016-12-04 15:33:13 -0500

    Fine Tunning

--- app/src/main/java/com/bignerdranch/android/taskmanager/AddLocationMapActivity.java
+++ app/src/main/java/com/bignerdranch/android/taskmanager/AddLocationMapActivity.java
@@ -1,9 +1,12 @@
 package com.bignerdranch.android.taskmanager;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
@@ -61,6 +64,13 @@ public class AddLocationMapActivity extends FragmentActivity implements OnMapRea
 
         // Add zoom buttons
         mUiSettings.setZoomControlsEnabled(true);
+        // Add my location button
+        mUiSettings.setMyLocationButtonEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        setUpViews();
         setUpViews();
     }
 
